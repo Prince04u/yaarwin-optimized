4c10715a97e86865ff05f9d938ef31bef443ebb5
prince mahendrakumar prajapati <2401030630043@silveroakuni.ac.in>
2026-06-09 11:55:32 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -81,7 +81,24 @@ function RootShell({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5TZVDXH1RC"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-5TZVDXH1RC');
+            `,
+          }}
+        />
       </head>
+
       <body>
         {children}
         <Scripts />
